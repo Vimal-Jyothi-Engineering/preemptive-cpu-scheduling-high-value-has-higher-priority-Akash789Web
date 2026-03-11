26aab5ccdbfec85cf52670be1055999c1f9fff60
Akash789Web <wilsonakash9@gmail.com>
2026-03-11 09:47:36 +0530

    Update priority.c

--- priority.c
+++ priority.c
@@ -9,13 +9,11 @@ struct process {
 int main() {
     int n, i;
 
-    printf("Enter number of processes: ");
     scanf("%d", &n);
 
     struct process p[n];
 
     for(i = 0; i < n; i++) {
-        printf("Enter PID, Arrival Time, Burst Time, Priority for process %d: ", i+1);
         scanf("%s %d %d %d", p[i].pid, &p[i].at, &p[i].bt, &p[i].pr);
         p[i].rt = p[i].bt;
     }
@@ -55,19 +53,20 @@ int main() {
     for(i = 0; i < n; i++) {
         p[i].tat = p[i].ct - p[i].at;
         p[i].wt = p[i].tat - p[i].bt;
+
         avg_wt += p[i].wt;
         avg_tat += p[i].tat;
     }
 
-    printf("\nWaiting Time:\n");
+    printf("Waiting Time:\n");
     for(i = 0; i < n; i++)
         printf("%s %d\n", p[i].pid, p[i].wt);
 
-    printf("\nTurnaround Time:\n");
+    printf("Turnaround Time:\n");
     for(i = 0; i < n; i++)
         printf("%s %d\n", p[i].pid, p[i].tat);
 
-    printf("\nAverage Waiting Time: %.2f\n", avg_wt/n);
+    printf("Average Waiting Time: %.2f\n", avg_wt/n);
     printf("Average Turnaround Time: %.2f\n", avg_tat/n);
 
     return 0;

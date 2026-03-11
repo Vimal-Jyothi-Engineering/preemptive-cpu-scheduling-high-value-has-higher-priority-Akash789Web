08c111416d18e24f800e3adba0c8149f880b7e16
Akash789Web <wilsonakash9@gmail.com>
2026-03-11 09:56:32 +0530

    Update priority.c

--- priority.c
+++ priority.c
@@ -7,67 +7,74 @@ struct process {
 };
 
 int main() {
-    int n, i;
 
-    scanf("%d", &n);
+    int n,i;
+
+    scanf("%d",&n);
 
     struct process p[n];
 
-    for(i = 0; i < n; i++) {
-        scanf("%s %d %d %d", p[i].pid, &p[i].at, &p[i].bt, &p[i].pr);
-        p[i].rt = p[i].bt;
+    for(i=0;i<n;i++){
+        scanf("%s %d %d %d",p[i].pid,&p[i].at,&p[i].bt,&p[i].pr);
+        p[i].rt=p[i].bt;
     }
 
-    int completed = 0, time = 0, idx;
-    int max_pr;
+    int completed=0,time=0,idx;
+    int min_pr;
 
-    while(completed < n) {
-        idx = -1;
-        max_pr = -1;
+    while(completed<n){
 
-        for(i = 0; i < n; i++) {
-            if(p[i].at <= time && p[i].rt > 0) {
-                if(p[i].pr > max_pr) {
-                    max_pr = p[i].pr;
-                    idx = i;
+        idx=-1;
+        min_pr=9999;
+
+        for(i=0;i<n;i++){
+            if(p[i].at<=time && p[i].rt>0){
+
+                if(p[i].pr < min_pr){
+                    min_pr=p[i].pr;
+                    idx=i;
                 }
             }
         }
 
-        if(idx != -1) {
+        if(idx!=-1){
+
             p[idx].rt--;
             time++;
 
-            if(p[idx].rt == 0) {
-                p[idx].ct = time;
+            if(p[idx].rt==0){
+                p[idx].ct=time;
                 completed++;
             }
-        } 
-        else {
+
+        }else{
             time++;
         }
     }
 
-    float avg_wt = 0, avg_tat = 0;
+    float avg_wt=0,avg_tat=0;
 
-    for(i = 0; i < n; i++) {
-        p[i].tat = p[i].ct - p[i].at;
-        p[i].wt = p[i].tat - p[i].bt;
+    for(i=0;i<n;i++){
 
-        avg_wt += p[i].wt;
-        avg_tat += p[i].tat;
+        p[i].tat=p[i].ct-p[i].at;
+        p[i].wt=p[i].tat-p[i].bt;
+
+        avg_wt+=p[i].wt;
+        avg_tat+=p[i].tat;
     }
 
     printf("Waiting Time:\n");
-    for(i = 0; i < n; i++)
-        printf("%s %d\n", p[i].pid, p[i].wt);
+
+    for(i=0;i<n;i++)
+        printf("%s %d\n",p[i].pid,p[i].wt);
 
     printf("Turnaround Time:\n");
-    for(i = 0; i < n; i++)
-        printf("%s %d\n", p[i].pid, p[i].tat);
 
-    printf("Average Waiting Time: %.2f\n", avg_wt/n);
-    printf("Average Turnaround Time: %.2f\n", avg_tat/n);
+    for(i=0;i<n;i++)
+        printf("%s %d\n",p[i].pid,p[i].tat);
+
+    printf("Average Waiting Time: %.2f\n",avg_wt/n);
+    printf("Average Turnaround Time: %.2f\n",avg_tat/n);
 
     return 0;
 }
